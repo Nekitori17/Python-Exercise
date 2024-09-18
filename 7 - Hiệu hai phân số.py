7b58d3ab017b88867a051ed193cb889ddbea1860
Nekitori Hakari <cuongnguyen286641@gmail.com>
2024-09-18 10:02:50 +0000

    feat(): trừ hai phân số

---
+++ 7 - Hiệu hai phân số.py
@@ -0,0 +1,35 @@
+import re
+from UTILS.get_output import *
+
+regex_phan_so = r"([+-]?\d+([+\-*/]\d+)*)\s+([+-]?\d+([+\-*/]\d+)*)"
+
+phan_so_1_dau_vao = str(input("Nhập số bị trừ (vd: 5 6): "))
+
+while not re.match(regex_phan_so, phan_so_1_dau_vao):
+  phan_so_1_dau_vao = str(input("Nhập lại số bị trừ (vd: 5 6): "))
+
+phan_so_1 = phan_so_thanh_tuple(phan_so_1_dau_vao, regex_phan_so)
+
+while phan_so_1[1] == 0:
+  phan_so_1_dau_vao = str(input("Vui lý số bị trừ. Không thể để mẫu bằng 0! (vd: 5 6): "))
+  phan_so_1 = phan_so_thanh_tuple(phan_so_1_dau_vao, regex_phan_so)
+
+phan_so_2_dau_vao = str(input("Nhập số trừ (vd: 5 6): "))
+
+while not re.match(regex_phan_so, phan_so_2_dau_vao):
+  phan_so_2_dau_vao = str(input("Nhập lại số trừ (vd: 5 6): "))
+
+phan_so_2 = phan_so_thanh_tuple(phan_so_2_dau_vao, regex_phan_so)
+
+while phan_so_2[1] == 0:
+  phan_so_2_dau_vao = str(input("Vui lý nhập lại so trừ. Không thể được mẫu bằng 0! (vd: 5 6): "))
+  phan_so_2 = phan_so_thanh_tuple(phan_so_2_dau_vao, regex_phan_so)
+
+
+if phan_so_1[1] == phan_so_2[1]:
+  ket_qua = tuple_hay_so(phan_so_1[0] - phan_so_2[0], phan_so_1[1])
+  in_ra_phan_so("Tổng hai phân số là: {0}", ket_qua, " ")
+else:
+  ket_qua = tuple_hay_so((phan_so_1[0] * phan_so_2[1]) - (phan_so_2[0] * phan_so_1[1]), phan_so_1[1] * phan_so_2[1])
+  in_ra_phan_so("Tổng hai phân số là: {0}", ket_qua, " ")
+
--- UTILS/get_output.py
+++ UTILS/get_output.py
@@ -2,9 +2,9 @@ from UTILS.fraction_operation import *
 from typing import *
 import re
 
-def in_ra_phan_so(text: str, object: any):
+def in_ra_phan_so(text: str, object: any, decimal: str = "/"):
   if isinstance(object, tuple):
-    print(str(text).format(f"{object[0]}/{object[1]}"))
+    print(str(text).format(f"{object[0]}{decimal}{object[1]}"))
   else:
     print(str(text).format(object))
 
@@ -16,8 +16,8 @@ def tuple_hay_so(so_bi_chia: int, so_chia: int) -> Tuple[int, int] | float:
 
 _regex_phan_so = r"([+-]?\d+([+\-*/]\d+)*)\/([+-]?\d+([+\-*/]\d+)*)"
 
-def phan_so_thanh_tuple(phan_so: str) -> Tuple[int, int] | None:
-    match = re.match(_regex_phan_so, phan_so)
+def phan_so_thanh_tuple(phan_so: str, regex: str = _regex_phan_so) -> Tuple[int, int] | None:
+    match = re.match(regex, phan_so)
     if match:
         return eval(match.group(1)), eval(match.group(3))
     return None
